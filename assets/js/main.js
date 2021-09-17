acbd6d34664bb3a823394d3eb7c68faf323a5743
isf3t <isfet@DESKTOP-AKF3P6C.localdomain>
2021-09-17 12:39:14 +0200

    fix-12

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,27 +2,15 @@
   // Theme switch
   const body = document.body;
   const lamp = document.getElementById("mode");
-  const sapienza_logo_dark = document.getElementById('dark_logo')
-  const sapienza_logo_light = document.getElementById('light_logo')
 
   const toggleTheme = (state) => {
     if (state === "dark") {
       localStorage.setItem("theme", "light");
       body.removeAttribute("data-theme");
 
-      if (sapienza_logo_light.display === 'none'){
-        sapienza_logo_light.display = 'block'
-        sapienza_logo_dark.display = 'none'
-      }
-
     } else if (state === "light") {
       localStorage.setItem("theme", "dark");
       body.setAttribute("data-theme", "dark");
-
-      if (sapienza_logo_dark.display === 'none'){
-        sapienza_logo_dark.display = 'block'
-        sapienza_logo_light.display = 'none'
-      }
 
     } else {
       initTheme(state);
